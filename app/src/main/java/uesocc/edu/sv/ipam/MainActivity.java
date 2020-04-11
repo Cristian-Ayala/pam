402a22feb83ac5ed40b331ea273df36588de5060
Cristian Ayala <cristianaaron10@gmail.com>
2020-04-11 15:09:58 -0600

    almost all operations are working

--- app/src/main/java/uesocc/edu/sv/ipam/MainActivity.java
+++ app/src/main/java/uesocc/edu/sv/ipam/MainActivity.java
@@ -1,12 +1,25 @@
 package uesocc.edu.sv.ipam;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PrimitiveIterator;
+
+import org.mozilla.javascript.Parser;
+import org.mozilla.javascript.ScriptableObject;
+import org.mozilla.javascript.ast.ExpressionStatement;
 
 public class MainActivity extends AppCompatActivity {
     Button btnCero, btnUno, btnDos, btnTres, btnCuatro, btnCinco, btnSeis, btnSiete, btnOcho, btnNueve;
@@ -134,58 +147,44 @@ public class MainActivity extends AppCompatActivity {
 
         btnSuma.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                reserva = Resultado.getText().toString();
-                operador = "+";
                 Resultado.setText(Resultado.getText().toString() + "+");
             }
         });
 
         btnResta.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                reserva = Resultado.getText().toString();
-                operador = "-";
                 Resultado.setText(Resultado.getText().toString() + "-");
             }
         });
 
         btnMultiplica.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                reserva = Resultado.getText().toString();
-                operador = "*";
-                Resultado.setText(Resultado.getText().toString() + "x");
+                Resultado.setText(Resultado.getText().toString() + "*");
             }
         });
 
         btnDivide.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                reserva = Resultado.getText().toString();
-                operador = "/";
                 Resultado.setText(Resultado.getText().toString() + "/");
             }
         });
 
         btnRaiz.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                reserva = Resultado.getText().toString();
-                operador = "\u221A";
-                Resultado.setText(Resultado.getText().toString() + "\u221A");
+                Resultado.setText(Resultado.getText().toString() + "^0.5");
             }
         });
 
         btnCuadrado.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                reserva = Resultado.getText().toString();
-                operador = "\u221A";
-                Resultado.setText(Resultado.getText().toString() + "\u00B2");
+                Resultado.setText(Resultado.getText().toString() + "^2");
             }
         });
 
 
         btnPunto.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                mostrar = Resultado.getText().toString();
-                mostrar = mostrar + ".";
-                Resultado.setText(mostrar);
+                Resultado.setText(Resultado.getText().toString() + ".");
             }
         });
 
@@ -202,32 +201,24 @@ public class MainActivity extends AppCompatActivity {
         btnBorrar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 mostrar = Resultado.getText().toString();
-                Resultado.setText(!mostrar.isEmpty()?mostrar.substring(0, mostrar.length() - 1):mostrar);
+                Resultado.setText(!mostrar.isEmpty() ? mostrar.substring(0, mostrar.length() - 1) : mostrar);
             }
         });
 
         btnIgual.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                mostrar = Resultado.getText().toString();
-                mostrar = mostrar + "1";
-                if (operador.equals("-")) {
-                    resultado = Double.parseDouble(reserva) - Double.parseDouble(Resultado.getText().toString().substring(reserva.length()+1, Resultado.getText().toString().length()));
-                    Ops.setText(String.valueOf(resultado));
-                    Resultado.setText("");
-                }
-                if (operador.equals("+")) {
-                    resultado = Double.parseDouble(reserva) + Double.parseDouble(Resultado.getText().toString().substring(reserva.length()+1, Resultado.getText().toString().length()));
-                    Ops.setText(String.valueOf(resultado));
-                    Resultado.setText("");
-                }
-                if (operador.equals("/")) {
-                    resultado = Double.parseDouble(reserva) / Double.parseDouble(Resultado.getText().toString().substring(reserva.length()+1, Resultado.getText().toString().length()));
-                    Ops.setText(String.valueOf(resultado));
-                    Resultado.setText("");
-                }
-                if (operador.equals("*")) {
-                    resultado = Double.parseDouble(reserva) * Double.parseDouble(Resultado.getText().toString().substring(reserva.length()+1, Resultado.getText().toString().length()));
-                    Ops.setText(String.valueOf(resultado));
+                org.mozilla.javascript.Context rhino = org.mozilla.javascript.Context.enter();
+                // turn off optimization to work with android
+                rhino.setOptimizationLevel(-1);
+                String res = Resultado.getText().toString().replaceAll("(-?\\d+(?:\\.\\d+)?)\\^(-?\\d+(?:\\.\\d+)?)", "Math.pow($1,$2)");
+
+                try {
+                    ScriptableObject scope = rhino.initStandardObjects();
+                    String result = rhino.evaluateString(scope, res, "JavaScript", 1, null).toString();
+                    Ops.setText(String.valueOf(result));
+
+                } finally {
+                    org.mozilla.javascript.Context.exit();
                     Resultado.setText("");
                 }
             }
